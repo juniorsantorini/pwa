7d06ba14097f183d60d6dff4c7d84b12022b0f90
juniorsantorini <144395275+juniorsantorini@users.noreply.github.com>
2026-02-03 02:25:01 -0300

    App bora pra cima
    App bora pra cima

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'borapracima-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativação do Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Removendo cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se disponível, senão busca da rede
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          // Não cachear se não for uma resposta válida
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          // Clona a resposta
+          const responseToCache = response.clone();
+          
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          
+          return response;
+        });
+      })
+  );
+});
